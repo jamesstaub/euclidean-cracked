6963d722d3fd7e88f153ee6fa8800ce3cf3355e8
James Staub <james@courbanize.com>
2017-12-08 06:19:51 -0500

    remove comment

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -4,26 +4,4 @@ import FirebaseAdapter from 'emberfire/adapters/firebase';
 
 export default FirebaseAdapter.extend({
   firebase: Ember.inject.service(),
-  // init() {
-  //   this._super(...arguments);
-  //
-  //   const userListRef = firebase.database().ref("USERS_ONLINE");
-  //   const myUserRef = userListRef.push();
-  //
-  //   firebase.database().ref(".info/connected")
-  //   .on("value", function (snap) {
-  //     if (snap.val()) {
-  //       // if we lose network then remove this user from the list
-  //       // myUserRef.onDisconnect()
-  //                // .remove();
-  //       // set user's online status
-  //       // setUserStatus("online");
-  //       console.log(userListRef);
-  //     } else {
-  //       // client has lost network
-  //       // setUserStatus("offline");
-  //     }
-  //   }
-  // );
-  // },
 });
