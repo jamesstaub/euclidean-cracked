af052dfb113112999181268b1b4fdde81f24acf4
James Staub <james@mapdwell.com>
2019-01-06 20:07:08 -0500

    bind space

--- app/components/audio-post-sequencer.js
+++ app/components/audio-post-sequencer.js
@@ -12,12 +12,25 @@ export default Component.extend({
 
   didInsertElement() {
     this.initSignalChain();
+    // document.body.addEventListener('keyup', this.onSpace.bind(this), true);
   },
 
   willDestroyElement() {
+    // document.body.removeEventListener('keyup', this.onSpace.bind(this), true);
     __.loop("stop");
     this.disconnectAll();
   },
+
+  // TODO prevent scrolling
+  // and properly removeEventListener
+  // onSpace(e) {
+  //   if (e.keyCode == 32) {
+  //     const action = this.isPlaying ? 'stop' : 'start';
+  //     this.send('loopAction', action);
+  //     e.preventDefault(); // dont go scrollin
+  //     return false;
+  //   }
+  // },
 
   disconnectAll() {
     // remove all existing cracked audio nodes
@@ -29,12 +42,12 @@ export default Component.extend({
     this.disconnectAll();
     // create a compressor -> DAC node for other nodes to connect to
     __()
-    .compressor({
-      release:.1,
-      id: 'master-compressor',
-      class:`post-${get(this, 'post.id')}`,
-    })
-    .dac(this.dacGain);
+      .compressor({
+        release: .1,
+        id: 'master-compressor',
+        class: `post-${get(this, 'post.id')}`,
+      })
+      .dac(this.dacGain);
   },
 
   actions: {
